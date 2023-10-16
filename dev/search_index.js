f83c6d01acbdabdc254f6f1ceebabc894782f128
Documenter.jl <documenter@juliadocs.github.io>
2023-10-16 21:20:16 +0000

    build based on f93fe8e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ChronoMakie","category":"page"},{"location":"#ChronoMakie","page":"Home","title":"ChronoMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ChronoMakie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ChronoMakie]","category":"page"}]
+}
