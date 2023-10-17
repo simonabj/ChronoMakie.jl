7467da908b957ae9b3bd14e0eb71b09a477f6389
Simon Andreas Bjørn <s.a.bjorn@ifi.uio.no>
2023-10-17 14:25:51 +0200

    Fix precision problems with format

--- src/ChronoMakie.jl
+++ src/ChronoMakie.jl
@@ -6,16 +6,24 @@ export DateAxis, TimeAxis, DateTimeAxis
 
 # Write your package code here.
 function DateAxis(args; dateformat = "dd-mm-yyyy" , kwargs...)
-    x_fmt = value -> Dates.format.(Date(0,12,31)+Day.(value), dateformat)
+    # Crete a clojure which runs on each tick value
+    x_fmt = values -> map( # The function maps each value to a string
+        value -> value % 1 == 0 ? # If the value is an integer
+            Dates.format.(Date(0,12,31)+Day.(value), dateformat) : # Use Date format
+            "", # else use an empty string
+        values
+    )
     return Axis(args; xtickformat = x_fmt, kwargs...)
 end
 
-function TimeAxis(args; timeformat = "HH:MM:SS", kwargs...)
+function TimeAxis(args; timeformat = "HH:MM:SS.sss", kwargs...)
+    # Allow time to use float, as the precision is smaller than milliseconds.
     x_fmt = value -> Dates.format.(Time(0)+Nanosecond.(value), timeformat)
     return Axis(args; xtickformat = x_fmt, kwargs...)
 end
 
 function DateTimeAxis(args; datetimeformat = "dd-mm-yyyy HH:MM:SS", kwargs...)
+    # Date time should be used when the precision is higher than seconds, and allow for floats as millis.
     x_fmt = value -> Dates.format.(DateTime(0,12,31)+Millisecond.(value.+62135683200000), datetimeformat)
     # x_fmt = value -> map(e->"$e", value)
     return Axis(args; xtickformat = x_fmt, kwargs...)
